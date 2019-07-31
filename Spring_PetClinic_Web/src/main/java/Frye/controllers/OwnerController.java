f5d85d8375497a60f208eea781d99fed58f9c577
Eric Wu <32679545+FriySenter@users.noreply.github.com>
2019-07-30 17:34:32 -0700

    Add files via upload

--- Spring_PetClinic_Web/src/main/java/Frye/controllers/OwnerController.java
+++ Spring_PetClinic_Web/src/main/java/Frye/controllers/OwnerController.java
@@ -2,31 +2,48 @@ package Frye.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import Frye.services.OwnerService;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class OwnerController {
 
-	private final OwnerService os;
+    private final OwnerService os;
 
-	public OwnerController(OwnerService os) {
-		super();
-		this.os = os;
-	}
+    public OwnerController(OwnerService os) {
+        super();
+        this.os = os;
+    }
 
-	@RequestMapping({ "/owners", "/owners/index", "owners/index.html" })
-	public String listOwners(Model model) {
+    @InitBinder
+    public void NotAllowFieds(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
 
-		model.addAttribute("Owner", os.findAll());
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String listOwners(Model model) {
 
-		return "owners/index";
-	}
+        model.addAttribute("Owner", os.findAll());
 
-	@RequestMapping({"/owners/find", "/oups"})
-	public String findOwners() {
+        return "owners/index";
+    }
 
-		return "notWorking";
-	}
+    @RequestMapping({"/owners/find", "/oups"})
+    public String findOwners() {
+
+        return "notWorking";
+    }
+
+    @GetMapping("/owners/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(os.findById(ownerId));
+        return mav;
+    }
 }
